5b0bd2e30ab0d48db2079bb7f460bc84a97147d3
Andrea Galvani <17992288+Terminator24@users.noreply.github.com>
2019-01-05 17:37:26 +0100

    Fixed SettingsManager and improvements.

--- Custom Control Collection/Settings/SettingsManager.cs
+++ Custom Control Collection/Settings/SettingsManager.cs
@@ -5,43 +5,43 @@ namespace CustomControlCollection.Settings
 {
     public class SettingsManager<T> where T : new()
     {
-        #region CREATE_OR_LOAD
+        #region CREATELOAD
         public static T CreateOrLoad(string filePath)
         {
-            CreateDirectory(filePath);
-
-            if (File.Exists(filePath))
-            {
-                return CreateOrLoad(File.OpenRead(filePath));
-            }
-            else
-            {
-                return (new T());
-            }
+            return (File.Exists(filePath) ? CreateOrLoad(File.OpenRead(filePath)) : new T());
         }
 
         public static T CreateOrLoad(Stream readStream)
         {
+            T genericSettings = default(T);
+
             if (readStream != null)
             {
                 using (StreamReader streamReader = new StreamReader(readStream))
                 {
-                    return ((T)new XmlSerializer(typeof(T)).Deserialize(streamReader));
+                    genericSettings = ((T)new XmlSerializer(typeof(T)).Deserialize(streamReader));
                 }
             }
             else
             {
-                return (new T());
+                genericSettings = new T();
             }
+
+            return genericSettings;
         }
         #endregion
 
         #region SAVE
         public void Save(string filePath)
         {
-            CreateDirectory(filePath);
+            string directoryName = Path.GetDirectoryName(filePath);
 
-            Save(File.OpenWrite(filePath));
+            if ((!string.IsNullOrEmpty(directoryName)) && (!Directory.Exists(directoryName)))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            Save(File.Create(filePath));
         }
 
         public void Save(Stream writeStream)
@@ -49,18 +49,6 @@ namespace CustomControlCollection.Settings
             using (StreamWriter streamWriter = new StreamWriter(writeStream))
             {
                 new XmlSerializer(typeof(T)).Serialize(streamWriter, this);
-            }
-        }
-        #endregion
-
-        #region DIRECTORY
-        private static void CreateDirectory(string filePath)
-        {
-            string directoryName = Path.GetDirectoryName(filePath);
-
-            if ((!string.IsNullOrEmpty(directoryName)) && (!Directory.Exists(directoryName)))
-            {
-                Directory.CreateDirectory(directoryName);
             }
         }
         #endregion
